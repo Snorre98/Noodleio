c5f73f2b853bb67450a6e52463559d7b9704cac8
Snorre Sæther <snosaet@gmail.com>
2025-02-11 09:24:58 +0100

    first commit

---
+++ ios/src/main/java/gr17/noodleio/game/IOSLauncher.java
@@ -0,0 +1,23 @@
+package gr17.noodleio.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import gr17.noodleio.game.Core;
+
+/** Launches the iOS (RoboVM) application. */
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration configuration = new IOSApplicationConfiguration();
+        return new IOSApplication(new Core(), configuration);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
