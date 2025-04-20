9a2c30eaa677dc7c2fe9c9a8edee461200a2ff15
Snorre Sæther <snosaet@gmail.com>
2025-04-20 03:47:57 +0200

    adds Food database table and kotlin model

--- core/src/main/java/gr17/noodleio/game/models/Food.kt
+++ core/src/main/java/gr17/noodleio/game/models/Food.kt
@@ -12,8 +12,24 @@ data class Food (
     val session_id: String,
     val x_pos: Int,
     val y_pos: Int,
-    val wasEaten: Boolean,
+    val was_eaten: Boolean,
 )
+
+
+/*
+*
+  create table public."Food" (
+  id uuid not null default gen_random_uuid (),
+  session_id uuid not null,
+  x_pos bigint not null,
+  y_pos bigint not null,
+  was_eaten boolean not null default false,
+  created_at timestamp with time zone not null default now(),
+  constraint Food_pkey primary key (id),
+  constraint Food_session_id_fkey foreign KEY (session_id) references "GameSession" (id) on delete CASCADE
+) TABLESPACE pg_default;
+*
+* */
 
 /*
 ---- DB function ----
